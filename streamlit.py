fcb5d482b882a6b831f9ea3507da3a075498c210
kylynfaye <kylyn.smith@yale.edu>
2024-04-18 19:13:55 -0400

    orbit and cone defaults can be plotted

--- streamlit.py
+++ streamlit.py
@@ -3,42 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ####################################
-from STAcc_v2.STAcc_v2.object_info.StarTracker import StarTracker
-from STAcc_v2.STAcc_v2.object_info.RSO import RSO
+from STAcc_v2.object_info.StarTracker import StarTracker
+from STAcc_v2.object_info.RSO import RSO
 from STAcc_v2.analyze.orbit_visualizer import *
 ####################################
 
 #if 'key' not in st.session_state: 
 #    st.session_state.key = 1
 
-st.title("STAcc_v2 Attempt")
+st.title("STAcc_v2 Beta Test")
 
 # @st.cache_data  # caches the result of the function below, not having to read in the data every time it reloads
-if st.checkbox("Upload Stuff"):
+STckr_default = StarTracker(datasheet="startracker1_datasheet.pdf")
+STckr_default.set_parameter_values()
 
+if st.checkbox("Upload custom star tracker instead"):
     datasheet = st.file_uploader('Upload the datasheet to your chosen commercial star tracker here, or use the default.', type='pdf', accept_multiple_files=False, label_visibility="visible")
-   
-    STckr = StarTracker(datasheet="startracker1_datasheet.pdf")
-    STckr.extract_text_from_pdf()
-    STckr.find_word_in_text()
+    STckr = StarTracker(datasheet=datasheet)
     STckr.set_parameter_values()
-    print(STckr.fov)
-
-    # if datasheet is not None:
-    #     import PyPDF2
-    #     reader = PyPDF2.PdfReader(datasheet)
-    #     number_of_pages = len(reader.pages)
-
-    #     pdf_text = ""
-
-    #     for i in range(number_of_pages):
-    #         page = reader.pages[i]
-    #         pdf_text += page.extract_text()
-    #         pdf_text += "\n"
-        
-    #     st.write(pdf_text)
-
-    st.write('hi')
+    st.write(STckr.fov)
 
 #@st.cache_data
 if st.checkbox("Get TLE"):
@@ -47,20 +30,21 @@ if st.checkbox("Get TLE"):
 
     username = st.text_input("Please type your username for Space-Track.org:")
     password = st.text_input("Please type your password for Space-Track.org:", type='password')
+    
+    Object_default = RSO()
+    Object_default.say_hello()
+    tle = Object_default.get_tle()
+    #st.write(tle)  # works!!
 
-    def download_tle(username=username, password=password, norad_cat_id=[25544, 41335]):
-        '''
-        Uses spacetrack package to query orbit information about any satellite from space-track.org database
-        Downloads TLE as .txt file named 'tle.txt' to working directory
-        '''
-        id = input("Type the ID of the satellite you would like to track with your star tracker. The default is [25544, 41335].")
-        stc = SpaceTrackClient(username, password)
-        tle = stc.tle_latest(norad_cat_id=id, ordinal=1, format='tle')
-        print(type(tle))
-        #with open("tle.txt", "w") as text_file:
-            #text_file.write(tle)
+    fig = plt.figure(figsize=(7,7))
+    ax = fig.add_subplot(111,projection='3d')
 
-    download_tle()
-    #open('tle.txt', 'r')
+    Object_default.orbit_plotter(ax)
+    cone_plotter(ax)
+
+    st.pyplot(fig)
+
+    #Object_default.orbit_plotter()
+
 
         #show_plot()
